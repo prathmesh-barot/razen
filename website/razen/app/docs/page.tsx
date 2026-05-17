c9d853bfd6f3ca40b927023db36899351a38dad0
prathmeshcodes-ai <prathmeshbarot2009@gmail.com>
2026-05-17 22:16:24 +0530

    refactor: move website to standalone razen-web repo
    - Remove website/ from Razen-CPP (now at github.com/razen-lang/razen-web)
    - Clean up .gitignore entries for website

--- website/razen/app/docs/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllDocs } from "@/lib/docs";
-import DocsViewer from "@/components/DocsViewer";
-
-export default function DocsPage() {
-  const docs = getAllDocs();
-  return <DocsViewer docs={docs} />;
-}
